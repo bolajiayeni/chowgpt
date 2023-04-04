90c75fece2f34409b17923e5b87030fb98965c92
Bolaji <ayenibolaji3@gmail.com>
2023-04-04 22:59:15 +0100

    Added components for the UI

---
+++ src/components/RecipeList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h2 class="text-2xl font-semibold mb-4 text-warm-gray-700">Recipes</h2>
+      <ul>
+        <li v-for="recipe in recipes" :key="recipe.id" class="mb-4 cursor-pointer" @click="$emit('select', recipe.id)">
+          <a :href="recipe.sourceUrl" target="_blank" rel="noopener noreferrer">
+            <div class="bg-white p-4 rounded-lg shadow-md">
+              <h3 class="text-xl font-semibold mb-2">{{ recipe.title }}</h3>
+              <img :src="recipe.image" :alt="recipe.title" class="w-full h-56 object-cover rounded-lg" />
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      recipes: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
